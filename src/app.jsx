4bb5d3f2da5e0177b66a3f859a433c946456cbb0
MarcioBADias <marcio.b.a.dias@gmail.com>
2025-01-19 22:51:20 -0300

    Estruturando a primeira pagina

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,22 @@
 const App = () => {
   return (
     <>
-      <h1>Boilerplace + React</h1>
+      <div className="app">
+        <main className="main">
+          <div>
+            <h4>Uma pergunta aqui</h4>
+            <div className="options">
+              <button className="btn btn-option">Opcao 1</button>
+              <button className="btn btn-option">Opcao 2</button>
+              <button className="btn btn-option">Opcao 3</button>
+              <button className="btn btn-option">Opcao 4</button>
+            </div>
+          </div>
+          <div>
+            <button className="btn btn-ui">Proxima</button>
+          </div>
+        </main>
+      </div>
     </>
   )
 }
