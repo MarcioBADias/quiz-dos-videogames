e4475bee978e9c4e47f14f9dc3110c17de500759
MarcioBADias <marcio.b.a.dias@gmail.com>
2025-01-19 22:59:14 -0300

    Comecando o header

--- src/app.jsx
+++ src/app.jsx
@@ -3,8 +3,12 @@ const App = () => {
     <>
       <div className="app">
         <main className="main">
+          <div className="app-header">
+            <img src="./img/logo-quiz-videogames.png" alt="Logo" />
+            <h1>Quiz dos Videogames</h1>
+          </div>
           <div>
-            <h4>Uma pergunta aqui</h4>
+            <h4>Qual foi o videogame?</h4>
             <div className="options">
               <button className="btn btn-option">Opcao 1</button>
               <button className="btn btn-option">Opcao 2</button>
