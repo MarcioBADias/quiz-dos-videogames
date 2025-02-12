ae3345c07406cb3d4ed9357c8aa608c38d932661
MarcioBADias <marcio.b.a.dias@gmail.com>
2025-02-12 02:20:22 -0300

    ✅ Separando o projeto em  pastas de componentes

--- src/app.jsx
+++ src/app.jsx
@@ -1,4 +1,11 @@
 import { useEffect, useReducer } from 'react'
+import { Timer } from '@/components/Timer'
+import { Header } from '@/components/Header'
+import { InitialScreen } from '@/components/InitialScreen'
+import { ResultScreen } from '@/components/ResultScreen'
+import { Questions } from '@/components/Questions'
+import { ProgressBar } from '@/components/ProgressBar'
+import { StepButtons } from '@/components/StepButtons'
 
 const secondsPerQuestion = 30
 
@@ -67,116 +74,6 @@ const initialState = {
   seconds: null,
 }
 
-const Timer = ({ appState }) => {
-  const minutes = Math.floor(appState.seconds / 60)
-  const secs = appState.seconds % 60
-
-  return (
-    <div className="timer">
-      {minutes < 10 ? `0${minutes}` : minutes} : {secs < 10 ? `0${secs}` : secs}
-    </div>
-  )
-}
-
-const Header = () => (
-  <header className="app-header">
-    <img src="./img/logo-quiz-videogames.png" alt="Logo" />
-    <h1>Quiz dos Videogames</h1>
-  </header>
-)
-
-const InitialScreen = ({ appState, onHandleClickStart }) => (
-  <div className="start">
-    <h2>Bem-vindo(a) ao Quiz dos Videogames!</h2>
-    <h3>{appState.apiData.length} Questões para te testar</h3>
-    <button onClick={onHandleClickStart} className="btn">
-      Bora começar
-    </button>
-  </div>
-)
-
-const ResultScreen = ({ appState, maxScore, onHandleClickRestart }) => {
-  const percentage = (appState.userScore / maxScore) * 100
-
-  return (
-    <>
-      <div className="result">
-        <span>
-          Voce fez <b>{appState.userScore}</b> pontos de {maxScore} (
-          {percentage}
-          %)
-        </span>
-      </div>
-      <button onClick={onHandleClickRestart} className="btn btn-ui">
-        Reiniciar o Quiz
-      </button>
-    </>
-  )
-}
-const Questions = ({ appState, onUserHasAnwered, onHandleClickOption }) => (
-  <div>
-    <h4>{appState.apiData[appState.currentQuestion].question}</h4>
-    <ul className="options">
-      {appState.apiData[appState.currentQuestion].options.map(
-        (option, index) => {
-          const answersClass = appState.clickedOption === index ? 'answer' : ''
-          const correctOrWrongClass = onUserHasAnwered
-            ? appState.apiData[appState.currentQuestion]?.correctOption ===
-              index
-              ? 'correct'
-              : 'wrong'
-            : ''
-          return (
-            <li key={option}>
-              <button
-                onClick={() => onHandleClickOption(index)}
-                className={`btn btn-option ${answersClass} ${correctOrWrongClass}`}
-                disabled={onUserHasAnwered}
-              >
-                {option}
-              </button>
-            </li>
-          )
-        },
-      )}
-    </ul>
-  </div>
-)
-
-const ProgressBar = ({ appState, maxScore, onUserHasAnwered }) => {
-  const progressValue = onUserHasAnwered
-    ? appState.currentQuestion + 1
-    : appState.currentQuestion
-  return (
-    <header className="progress">
-      <label>
-        <progress max={appState.apiData.length} value={progressValue}>
-          {progressValue}
-        </progress>
-        <span>
-          Questao <b>{appState.currentQuestion + 1}</b> de{' '}
-          {appState.apiData.length}
-        </span>
-        <span>
-          <b>
-            {appState.userScore} / {maxScore} pontos
-          </b>
-        </span>
-      </label>
-    </header>
-  )
-}
-
-const StepButtons = ({ appState, onHandleClickNextQuestion }) => (
-  <div>
-    <button onClick={onHandleClickNextQuestion} className="btn btn-ui">
-      {appState.currentQuestion === appState.apiData.length - 1
-        ? 'Finalizar'
-        : 'Proxima'}
-    </button>
-  </div>
-)
-
 const App = () => {
   const [state, dispatch] = useReducer(reduce, initialState)
 
@@ -193,21 +90,17 @@ const App = () => {
     if (state.seconds === null) {
       return
     }
-    const id = setTimeout(() => dispatch({ type: 'tick' }), 10)
+    const id = setTimeout(() => dispatch({ type: 'tick' }), 1000)
 
     return () => clearTimeout(id)
   }, [state.seconds])
 
   const handleClickStart = () => dispatch({ type: 'clicked_start' })
-
   const handleClickOption = (index) =>
     dispatch({ type: 'clicked_option', index })
-
   const handleClickNextQuestion = () =>
     dispatch({ type: 'clicked_next_question' })
-
   const handleClickRestart = () => dispatch({ type: 'clicked_restart' })
-
   const userHasAnwered = state.clickedOption !== null
   const maxScore = state.apiData.reduce((acc, q) => acc + q.points, 0)
 
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Header = () => (
+  <header className="app-header">
+    <img src="./img/logo-quiz-videogames.png" alt="Logo" />
+    <h1>Quiz dos Videogames</h1>
+  </header>
+)
+
+export { Header }
